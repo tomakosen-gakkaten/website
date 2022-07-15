26f2bf89da2af38f1e32e74005386b3d4141e137
arkmirom <78552299+arkmirom@users.noreply.github.com>
2022-07-15 17:58:29 +0900

    Update Deco.tsx

--- src/pages/group/Deco.tsx
+++ src/pages/group/Deco.tsx
@@ -4,6 +4,16 @@ export const Deco = () => {
   return (
     <div id="deco" className="group">
       <h2>装飾班</h2>
+      <p>
+      装飾班では当日の情報棟の装飾や案内，入り口に展示する置物の修復・制作，
+        プロジェクションマッピングなどを行います．
+      </p>
+
+      <div className="img">
+        <img src={ProjectionMapping} alt="プロジェクションマッピング" loading="lazy" />
+        <p>プロジェクションマッピング</p>
+      </div>
+      
       <BackToTop />
     </div>
   );
