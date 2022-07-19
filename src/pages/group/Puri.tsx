de0a155f96ec20bc989efedd4ef1228964c3134b
yousaypen <58547503+yousaypen@users.noreply.github.com>
2022-07-19 13:18:08 +0900

    プリクラの画像を追加

--- src/pages/group/Puri.tsx
+++ src/pages/group/Puri.tsx
@@ -11,8 +11,12 @@ export const Puri = () => {
         プリクラのソフトウェアでは、まずPCのカメラで写真を撮り、その写真に落書きを書き込むことが出来ます。その後、QRコードで完成した写真を読み取ることもできます。
       </p>
       <p>
-        その他、追加機能の開発も検討しています。
+        その他、スタンプ等の追加機能の開発も検討しています。
       </p>
+      <div className="img">
+        <img src="./img/purikura_sample.png" alt="プリクラの画面" loading="lazy" />
+        <p>現段階のプリクラの実際の画面</p>
+      </div>
       <BackToTop />
     </div>
   );
